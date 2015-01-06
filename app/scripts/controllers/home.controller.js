b3ecbc43eef3fb7090f2b2bbd3dbfbd271a0edc1
cyurtbil <cenky92@gmail.com>
2015-01-06 12:31:55 -0500

    user successfully logs in and out with soundcloud

--- app/scripts/controllers/home.controller.js
+++ app/scripts/controllers/home.controller.js
@@ -1,8 +1,21 @@
 'use strict';
 
-app.controller('HomeController', ['$http', 'ServerUrl', '$scope', function($http, ServerUrl, $scope) {
+app.controller('HomeController', ['$http', 
+                                  'ServerUrl', 
+                                  '$scope', 
+                                  '$location',
+                                  '$window',
+                                  function($http, ServerUrl, $scope, $location, $window) {
 
-  // $http.get(ServerUrl + 'cloud/get_current_user').success(function(response) {
-  //   console.log(response);
-  // });
+  $http.get(ServerUrl + 'users/get_current_user').success(function(response) {
+    $window.sessionStorage.setItem('MusicWallet.user', response.current_user.token);
+    $http.defaults.headers.common['Authorization'] = 'Token token=' + $window.sessionStorage.getItem('MusicWallet.user');
+  });
+
+  $scope.logout = function() {
+    $http.get(ServerUrl + 'logout').success(function(response) {
+      $window.sessionStorage.removeItem('MusicWallet.user');
+      $location.path('/');
+    });
+  }
 }]);
