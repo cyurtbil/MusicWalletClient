5b5b9f000b7b22c1ff7c38ed2ba41b0049e622a1
cyurtbil <cenky92@gmail.com>
2015-01-10 19:27:47 -0500

    refactored the wallet name display with walletFactory

--- app/scripts/controllers/home.controller.js
+++ app/scripts/controllers/home.controller.js
@@ -4,19 +4,14 @@ app.controller('HomeController', ['$scope',
                                   'authFactory',
                                   '$location',
                                   'dataFactory',
-                                  function($scope, authFactory, $location, dataFactory) {
+                                  'walletFactory',
+                                  function($scope, authFactory, $location, dataFactory, walletFactory) {
 
   $(document).ready(function() {
     $('body').removeClass('bg');
   });
 
   dataFactory.fetchWallets().then(function(response) {
-    var wallets = response.data.wallets;
-    var walletNames = [];
-    for(var i = 0; i < wallets.length; i++) {
-      walletNames.push(wallets[i].name);
-    };
-
-    $scope.uniqueWalletNames = $.unique(walletNames);
+    $scope.uniqueWalletNames = walletFactory.createUniqueWalletNamesArray(response.data.wallets);
   });
 }]);
--- app/scripts/factories/WalletFactory.js
+++ app/scripts/factories/WalletFactory.js
@@ -1,5 +1,17 @@
 'use strict';
 
 app.factory('walletFactory', [function() {
-  
+
+  var createUniqueWalletNamesArray = function(wallets) {
+    var walletNames = [];
+    for(var i = 0; i < wallets.length; i++) {
+      walletNames.push(wallets[i].name);
+    };
+
+    return $.unique(walletNames);
+  };
+
+  return {
+    createUniqueWalletNamesArray: createUniqueWalletNamesArray
+  };
 }]);
