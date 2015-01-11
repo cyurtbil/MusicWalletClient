7f7ddebb4d69e168d33c7c5fe301c6121245caa6
cyurtbil <cenky92@gmail.com>
2015-01-10 19:22:51 -0500

    displayed the wallet names succesfully on homepage

--- app/scripts/controllers/home.controller.js
+++ app/scripts/controllers/home.controller.js
@@ -3,9 +3,20 @@
 app.controller('HomeController', ['$scope',
                                   'authFactory',
                                   '$location',
-                                  function($scope, authFactory, $location) {
+                                  'dataFactory',
+                                  function($scope, authFactory, $location, dataFactory) {
 
   $(document).ready(function() {
     $('body').removeClass('bg');
+  });
+
+  dataFactory.fetchWallets().then(function(response) {
+    var wallets = response.data.wallets;
+    var walletNames = [];
+    for(var i = 0; i < wallets.length; i++) {
+      walletNames.push(wallets[i].name);
+    };
+
+    $scope.uniqueWalletNames = $.unique(walletNames);
   });
 }]);
--- app/scripts/factories/DataFactory.js
+++ app/scripts/factories/DataFactory.js
@@ -2,4 +2,11 @@
 
 app.factory('dataFactory', ['$http', 'ServerUrl', function($http, ServerUrl) {
 
+  var fetchWallets = function() {
+    return $http.get(ServerUrl + 'wallets.json');
+  };
+
+  return {
+    fetchWallets: fetchWallets
+  };
 }]);
