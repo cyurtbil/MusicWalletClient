b3d6f4f0f431498688aa4442ad48185bb7bbfb7e
cyurtbil <cenky92@gmail.com>
2015-01-13 12:22:48 -0500

    now the songs of specific wallet of all users shows app on click ot tags on wallets name

--- app/scripts/controllers/home.controller.js
+++ app/scripts/controllers/home.controller.js
@@ -5,13 +5,29 @@ app.controller('HomeController', ['$scope',
                                   '$location',
                                   'dataFactory',
                                   'walletFactory',
-                                  function($scope, authFactory, $location, dataFactory, walletFactory) {
+                                  '$sce',
+                                  function($scope, authFactory, $location, dataFactory, walletFactory, $sce) {
 
   $(document).ready(function() {
     $('body').removeClass('bg');
   });
 
   dataFactory.fetchWallets().then(function(response) {
+    $scope.wallets = response.data.wallets;
     $scope.uniqueWalletNames = walletFactory.createUniqueWalletNamesArray(response.data.wallets);
   });
+
+  $scope.viewSongsOfAll = function(walletName) {
+    var clickedWallet = $scope.wallets.filter(function(wallet) {return wallet.name === walletName;});
+    $scope.walletSongs = [];
+    clickedWallet.forEach(function(wallet) {
+      wallet.songs.forEach(function(song) {
+        $scope.walletSongs.push(song);
+      });
+    });
+  };
+
+  $scope.fixUrl = function(source) {
+    return $sce.trustAsResourceUrl(source);
+  };
 }]);
