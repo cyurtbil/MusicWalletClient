a924f1a8fb7c75f1269045356df34c3d1834c57f
cyurtbil <cenky92@gmail.com>
2015-01-16 15:33:37 -0500

    change the registeration process for users

--- app/scripts/controllers/connect.controller.js
+++ app/scripts/controllers/connect.controller.js
@@ -1,34 +1,18 @@
 'use strict';
 
-app.controller('ConnectController', ['$scope', 'authFactory', '$http', 'ServerUrl', '$window', 'dataFactory', 'userFactory', function($scope, authFactory, $http, ServerUrl, $window, dataFactory, userFactory) {
+app.controller('ConnectController', ['$scope',
+                                     'authFactory',
+                                     '$http',
+                                     'ServerUrl',
+                                     '$window',
+                                     'dataFactory',
+                                     'userFactory',
+                                     function($scope, authFactory, $http, ServerUrl, $window, dataFactory, userFactory) {
 
 
   $(document).ready(function() {
     $('body').addClass('bg');
   });
 
-
-  dataFactory.fetchUsers().then(function(response) {
-    $scope.currentUser = userFactory.findCurrentUser(response.data.users);
-    if(!!$scope.currentUser) {
-      authFactory.logout($scope.currentUser).then(function() {
-        $window.sessionStorage.removeItem('MusicWallet.user');
-      });
-    }
-  });
-
-
-  authFactory.connect().then(function(response) {
-    $scope.connect = response.data.url;
-  });
-
-  $scope.connectToCloud = function(event) {
-    event.preventDefault();
-    $http.get(ServerUrl + 'users/create_different').success(function(response) {
-      $window.sessionStorage.setItem('MusicWallet.user', response.token);
-      $http.post(ServerUrl + 'users/store_user', {info: response}).success(function() {
-        window.location.href = $scope.connect;
-      });
-    });
-  };
+  
 }]);
