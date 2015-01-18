62040536f1e32c12b93184b20ebc63b951238bef
cyurtbil <cenky92@gmail.com>
2015-01-18 14:13:31 -0500

    now whwn user presses to see his her wallets it will activate walletactive class

--- app/scripts/controllers/profile.controller.js
+++ app/scripts/controllers/profile.controller.js
@@ -9,12 +9,17 @@ app.controller('ProfileController', ['$scope',
 
   dataFactory.fetchUsers().then(function(response) {
     $scope.currentUser = userFactory.findCurrentUser(response.data.users);
+    $scope.currentUser.wallets.forEach(function(wallet) {
+      wallet.active = false;
+    });
   });
 
-  $scope.viewSongs = function(wallet) {
-    $('h1').show(500);
+  $scope.viewSongs = function(wallet, wallets) {
+    wallets.forEach(function(element) {
+      element.active = false;
+    });
+    wallet.active = true;
     walletFactory.getWallet(wallet).then(function(response) {
-      $scope.wallet = response.data;
       $scope.walletSongs = response.data.songs;
     });
   }; 
@@ -22,5 +27,4 @@ app.controller('ProfileController', ['$scope',
   $scope.fixUrl = function(source) {
     return $sce.trustAsResourceUrl(source);
   };
-
 }]);
