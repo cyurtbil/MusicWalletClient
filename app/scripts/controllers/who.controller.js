aa6a130704f21a2bb691068f54e25fc82ac39c1c
cyurtbil <cenky92@gmail.com>
2015-01-21 19:08:55 -0500

    added underscorejs to find the maximum songs of the wallet that usr has

--- app/scripts/controllers/who.controller.js
+++ app/scripts/controllers/who.controller.js
@@ -1,5 +1,15 @@
 'use strict';
 
-app.controller('WhoController', [function() {
+app.controller('WhoController', ['$scope', 'dataFactory', function($scope, dataFactory) {
+
+  dataFactory.fetchUsers().then(function(response) {
+    $scope.users = response.data.users;
+  });
+
+  $scope.getMaxUserWallet = function(user) {
+    var highestSongCountUserWallet = _.max(user.wallets, function(wallet) { return wallet.song_count;});
+    return [highestSongCountUserWallet.name, highestSongCountUserWallet.song_count];
+  };
+
 
 }]);
