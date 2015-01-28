3473420e9d6b23d8b94814231488e290d920d21b
cyurtbil <cenky92@gmail.com>
2015-01-28 14:15:14 -0500

    edits to mobile desgin

--- app/scripts/controllers/navbar.controller.js
+++ app/scripts/controllers/navbar.controller.js
@@ -17,4 +17,14 @@ app.controller('NavbarController', ['$scope',
       $window.sessionStorage.removeItem('MusicWallet.user');
     });
   };
+
+  $scope.toggleNavbar = function() {
+    $('#collapse').stop(true, true).slideToggle(300);
+  };
+
+  $scope.changeHamburger = function() {
+    $('#hamburger .line:eq(0)').toggleClass('line1');
+    $('#hamburger .line:eq(1)').toggleClass('line2');
+    $('#hamburger .line:eq(2)').toggleClass('line3');
+  };
 }]);
