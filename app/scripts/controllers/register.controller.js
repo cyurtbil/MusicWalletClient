fb919f92e8278e058aeca19ed8d4981a376dfb12
cyurtbil <cenky92@gmail.com>
2015-01-29 10:49:13 -0500

    added errors to registration page

--- app/scripts/controllers/register.controller.js
+++ app/scripts/controllers/register.controller.js
@@ -12,14 +12,25 @@ app.controller('RegisterController', ['$scope',
   });
 
   $scope.doesPasswordsMatch = true;
+  $scope.doesExist = true;
 
   $scope.registerUser = function(user) {
     var userParams = {user: user};
 
     if(user.password === user.password_confirmation) {
-      userFactory.registerUser(userParams).then(function(response) {
-        authFactory.createUserSession(response.data);
-        $location.path('/');
+      dataFactory.fetchUsers().then(function(response) {
+        var existingUser = response.data.users.filter(function(element) {return element.username === user.username})[0];
+        if(!existingUser) {
+          userFactory.registerUser(userParams).then(function(response) {
+            authFactory.createUserSession(response.data);
+            $location.path('/');
+          });
+        } else {
+          $scope.existingUsername = existingUser.username;
+          $scope.doesExist = false;
+          $('#existing-error').slideDown(200);
+          $('#existing-error').delay(3000).slideUp(200);
+        };
       });
     } else {
       $scope.doesPasswordsMatch = false;
